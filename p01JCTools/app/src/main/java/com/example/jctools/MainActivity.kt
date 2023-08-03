644ffa4df0884381ed9a666e814812bfe5109669
Joey <yty0606@gmail.com>
2023-08-03 23:04:33 +0800

    Feat: Join navigation(2)
    Solution:
    1. As title.
    2. Some code still need to study and add more comment in the next commit.
    
    Finder: Joey
    Author: Joey

--- p01JCTools/app/src/main/java/com/example/jctools/MainActivity.kt
+++ p01JCTools/app/src/main/java/com/example/jctools/MainActivity.kt
@@ -4,14 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.jctools.ui.theme.JctoolsTheme
+import com.example.jctools.view.ScreenSyntax
+import com.example.jctools.view.ScreenUI
 
+/*
+    Author: Joey yang
+    Date: 2023/08/03
+    Purpose: Jetpack UI and Syntax test environment
+    Reference website:
+      1. https://play.kotlinlang.org/byExample/overview
+      2. https://foso.github.io/Jetpack-Compose-Playground/
+
+    Program reference:
+      PR-01: https://developer.android.com/jetpack/compose/navigation?hl=zh-tw
+
+    Attention:
+      1. 研究: 即要進行研究,了解內容後再上傳
+ */
+
+sealed class Destination(val route: String) {
+    object Syntax: Destination("syntax")
+    object UI: Destination("ui")
+}
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +47,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController() //看上方 PR-01
+                    ToolsScaffold(navController = navController) //重要,是在 MainActivity 外面
+
                 }
             }
         }
@@ -30,14 +57,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun ToolsScaffold(navController: NavHostController) {
+    val scaffoldState = rememberScaffoldState() //研究
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JctoolsTheme {
-        Greeting("Android")
+    Scaffold(
+        scaffoldState = scaffoldState,
+        bottomBar = {}
+    ) { paddingValues ->
+        NavHost(navController = navController, startDestination = Destination.UI.route) {
+            //重要: 有一個 Interface NavHost does not have constructors的錯誤,解法要加上 import androidx.navigation.compose.NavHost
+            composable(Destination.UI.route) {
+                ScreenUI()
+            }
+            composable(Destination.Syntax.route) {
+                ScreenSyntax()
+            }
+        }
     }
 }
---
+++ p01JCTools/app/src/main/java/com/example/jctools/view/ScreenSyntax.kt
@@ -0,0 +1,9 @@
+package com.example.jctools.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ScreenSyntax() {
+    Text(text = "Syntax screen")
+}
